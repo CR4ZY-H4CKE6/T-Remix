f300d3e9f57ea53f6d72301892a9aeee6097b433
CR4ZY-H4CKE6 <89270464+CR4ZY-H4CKE6@users.noreply.github.com>
2021-08-21 16:00:24 +0530

    Update t-remix.sh

--- T-Remix-master/t-remix.sh
+++ T-Remix-master/t-remix.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 ######################################
 #        PROJECT: T-Remix            #
-#        Author: ARIJ ARMAN          #
+#        Author: CR4ZY H4CKE6 (AKSHAY)          #
 #  email: mrxanonymous633@gmailcom   #
 ######################################
 
@@ -50,7 +50,7 @@ echo "
 echo " "
 echo " "
 echo " 
-       <─────────────── [ Re-Coded by =*•.¸♡ARIJ ARMAN♡¸.•* ] ───────────────>            
+       <─────────────── [ Re-Coded by =*•.¸♡CRAZY_HACKER♡¸.•* ] ───────────────>            
                                                                " |lolcat
 
 
@@ -173,7 +173,7 @@ echo " "
 
 echo " "
 echo "
-        <─────────────── [Re-Coded by *•.¸♡ARIJ ARMAN♡¸.•* ] ───────────────>              
+        <─────────────── [Re-Coded by *•.¸♡CRAZY_HACKER♡¸.•* ] ───────────────>              
                             " |lolcat
 
 echo " "
@@ -187,27 +187,27 @@ sleep 3.0
 clear
 tput clear
 tput cup 3 30
-echo -e "\e[44m Like \e[41m Subscribe \e[m"
+echo -e "\e[44m HELLO \e[41m HOW ARE YOU \e[m"
 tput cup 6 30
-echo -e "\x1b[42m ARIJ ARMAN's TECH KNOW LINUX channel \x1b[m"
+echo -e "\x1b[42m CR4ZY H4CKE6 (AKSHAY) \x1b[m"
 tput cup 9 0
 echo -en "\e[93m what is your name : \e[m"
 read answer
 tput cup 12 22
 echo -e "\e[104m hello! Open a New session!! $answer! \e[m"
 tput cup 15 0
-echo -en "\e[93m Have you subscribe my channel for hacking tutorial (y/n)? \e[m "
+echo -en "\e[93m HAVE YOU LIKE MY THIS TOOL (y/n)? \e[m "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 tput cup 17 21
-    echo -e "\e[42m Thankyou for subscribing my channel \e[m"
+    echo -e "\e[42m Thanks For Using my tool \e[m"
 else
 tput cup 17 23
-    echo -e "\x1b[41m Please subsrcibe my channel for hacking tutorial \x1b[m"
+    echo -e "\x1b[41m TASTE YOUR VIRTUAL WORLD \x1b[m"
 fi
 tput cup 20 0
-figlet -f slant "ARIJ ARMAN's TECH KNOW LINUX" |lolcat
+figlet -f slant "CRAZY_HACKER" |lolcat
 echo " "
 echo " "
 #done setting up
