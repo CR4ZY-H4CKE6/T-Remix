a06a794ef02cb7abdc5a7f32e8e5a7372875b67a
CR4ZY-H4CKE6 <89270464+CR4ZY-H4CKE6@users.noreply.github.com>
2021-08-21 16:10:26 +0530

    Update remove-banner.sh

--- remove-banner.sh
+++ remove-banner.sh
@@ -18,27 +18,27 @@ cd $HOME
 
 tput clear
 tput cup 3 30
-echo -e "\e[44m Like \e[41m Subscribe \e[m"
+echo -e "\e[44m HELLO \e[41m HOW ARE YOU \e[m"
 tput cup 6 30
-echo -e "\x1b[42m TECH KNOW LINUX by ARIJ ARMAN \x1b[m"
+echo -e "\x1b[42m CR4ZY H4CKE6 (AKSHAY) \x1b[m"
 tput cup 9 0
 echo -en "\e[93m what is your name : \e[m"
 read answer
 tput cup 12 22
 echo -e "\e[104m hello! Have a nice day!! $answer! \e[m"
 tput cup 15 0
-echo -en "\e[93m Have you subscribe my channel TECH KNOW LINUX for more hacking videos (y/n)? \e[m "
+echo -en "\e[93m HAVE YOU LIKE MY THIS TOOL (y/n)? \e[m "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 tput cup 17 21
-    echo -e "\e[42m Thank you for subscribing my channel \e[m"
+    echo -e "\e[42m Thanks For Using my tool \e[m"
 else
 tput cup 17 23
-    echo -e "\x1b[41m Please subsrcibe channel for hacking videos \x1b[m"
+    echo -e "\x1b[41m TASTE YOUR VIRTUAL WORLD \x1b[m"
 fi
 tput cup 20 0
-figlet -f slant "TECH KNOW LINUX by ARIJ ARMAN" |lolcat
+figlet -f slant "CRAZY_HACKER" |lolcat
 echo " "
 echo " "
 
